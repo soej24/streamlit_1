127e3a2f6fb5e9642eac26bb7f0b8a70562dc44a
soej24 <soej24@naver.com>
2022-05-23 01:26:15 +0900

    add

--- soej_project1.py
+++ soej_project1.py
@@ -1,10 +1,21 @@
 import streamlit as st
+from soej_project1_home import run_home
 
 def main() :
+  
+    st.title('인천지역 체육관 찾기 어플')
+
+    menu = ['Home', 'About','지역별 검색','종목별 검색','시합일정','EDA','ML']
+    choice = st.sidebar.selectbox('메뉴',menu)
     
-    # 비밀번호 입력
-    password = st.text_input('비밀번호 입력', type='password')
-    st.write(password)
+    if choice == menu[0]:
+        
+    elif choice == menu[1]:
+       pass
+    elif choice == menu[2]:
+       pass
+    elif choice == menu[3]:
+        pass
 
 if __name__ == "__main__" :
     main()
